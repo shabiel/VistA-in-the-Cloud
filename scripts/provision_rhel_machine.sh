e80d100b941623b014b7c70525cc1188c0e93dbb
Sam Habiel <sam.habiel@gmail.com>
2018-07-03 14:18:27 -0400

    Change to use VEHU

--- scripts/provision_rhel_machine.sh
+++ scripts/provision_rhel_machine.sh
@@ -9,5 +9,4 @@ mkdir -p /mnt/resource/docker /etc/docker
 echo '{"data-root": "/mnt/resource/docker"}' > /etc/docker/daemon.json
 systemctl start docker
 cd /mnt/resource
-docker pull osehra/osehravista
-docker run -p 9430:9430 -p 8001:8001 -p9080:9080 -p2222:22 -p57772:57772 -d -P --name=cache osehra/osehravista
+docker run -p 9430:9430 -p 8001:8001 -p9080:9080 -p2222:22 -p57772:57772 -d -P --name=vehu osehra/vehu
